e343d15bcf06075c7077f5c16a26695c7554652c
Goncalo Pereira <github@goncalopereira.com>
2020-06-05 17:44:18 +0100

    accounts to own file

--- internal/data/account/account.go
+++ internal/data/account/account.go
@@ -15,17 +15,6 @@ type Account struct {
 	Data         `json:"data"`
 }
 
-type Accounts struct {
-	data.IOutput `json:"omitempty"`
-	fmt.Stringer `json:"accountsignore,omitempty"`
-	Data         *[]Data `json:"data"`
-}
-
-func (a *Accounts) String() string {
-	account, _ := json.DataToBody(a)
-	return string(account)
-}
-
 func NewAccount(id string, organisationID string, attributes Attributes) *Account {
 	a := &Account{}
 	a.Data = newData(id, organisationID, attributes)
