8683cb4d119efc14f16391e89d5001a7fb7582c7
Goncalo Pereira <github@goncalopereira.com>
2020-06-07 12:01:18 +0100

    small fix for docker

--- scripts/tasks/e2e.sh
+++ scripts/tasks/e2e.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-./build/ci/wait-for-it.sh localhost:8080 -- echo 'ready for e2e!'
+./build/ci/wait-for-it.sh "${API_HOST}":"${API_PORT}" -- echo 'ready for e2e!'
 go test -v  ./test/e2e/... -count 1
